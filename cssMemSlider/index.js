a95a73f371f7ab4ee336d8c77164c774628c140e
Macbaren <macbaren123@gmail.com>
2022-05-16 23:31:14 +0300

    feat: add mobile width (16 май 2022 г. 23:31:13)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -1,7 +1,4 @@
-const TEXT_A = document.querySelector('#item-a');
-const TEXT_B = document.querySelector('#item-b');
-const TEXT_C = document.querySelector('#item-c');
-const TEXT_D = document.querySelector('#item-d');
+const BUTTON_A = document.querySelector('#button-a');
 
 const INPUTS = document.querySelectorAll('.state');
 
@@ -10,90 +7,50 @@ const TEXTS = document.querySelectorAll('.text-item');
 const CAROUSEL_WINDOW = document.querySelector('.meme__items');
 
 const MEME_TEXT = document.querySelector('.meme-text');
+let widthContent = 900;
 
-INPUTS.forEach((inp) =>
-  inp.addEventListener('click', (event) => {
-    switch (event.target.id) {
-      case 'button-a':
-        CAROUSEL_WINDOW.style.marginLeft = '0';
-        MEME_TEXT.style.marginLeft = '0';
-        break;
-      case 'button-b':
-        CAROUSEL_WINDOW.style.marginLeft = '-1280px';
-        MEME_TEXT.style.marginLeft = '-930px';
+const render = () => {
+  INPUTS.forEach((inp) =>
+    inp.addEventListener('click', (event) => {
+      switch (event.target.id) {
+        case 'button-a':
+          CAROUSEL_WINDOW.style.marginLeft = '0';
+          MEME_TEXT.style.marginLeft = '0';
+          break;
+        case 'button-b':
+          CAROUSEL_WINDOW.style.marginLeft = `-${widthContent}px`;
+          MEME_TEXT.style.marginLeft = `-${widthContent}px`;
+          break;
+        case 'button-c':
+          CAROUSEL_WINDOW.style.marginLeft = `-${widthContent * 2}px`;
+          MEME_TEXT.style.marginLeft = `-${widthContent * 2}px`;
+          break;
+        case 'button-d':
+          CAROUSEL_WINDOW.style.marginLeft = `-${widthContent * 3}px`;
+          MEME_TEXT.style.marginLeft = `-${widthContent * 3}px`;
+          break;
+        default:
+          break;
+      }
+    })
+  );
+};
 
-        break;
-      case 'button-c':
-        CAROUSEL_WINDOW.style.marginLeft = '-2560px';
-        MEME_TEXT.style.marginLeft = '-1860px';
+window.addEventListener('resize', (event) => {
+  console.log(event.target.innerWidth);
+  if (event.target.innerWidth < 1280) {
+    widthContent = 300;
+    CAROUSEL_WINDOW.style.marginLeft = '0';
+    MEME_TEXT.style.marginLeft = '0';
+    BUTTON_A.checked = true;
+    render();
+  } else {
+    widthContent = 900;
+    CAROUSEL_WINDOW.style.marginLeft = '0';
+    MEME_TEXT.style.marginLeft = '0';
+    BUTTON_A.checked = true;
+    render();
+  }
+});
 
-        break;
-      case 'button-d':
-        CAROUSEL_WINDOW.style.marginLeft = '-3840px';
-        MEME_TEXT.style.marginLeft = '-2790px';
-
-        break;
-
-      default:
-        break;
-    }
-    console.log('event', event.target.id);
-  })
-);
-
-// const BTN_LEFT = document.querySelector('#btn-left');
-// const BTN_RIGHT = document.querySelector('#btn-right');
-// const CAROUSEL = document.querySelector('#carousel');
-
-// let ITEMS_LEFT = document.querySelector('#items-left');
-// let ITEMS_RIGHT = document.querySelector('#items-right');
-// let ITEMS_ACTIVE = document.querySelector('#items-active');
-
-// let PET_BUTTONS;
-// const PETS_ID_ARR = [0, 1, 2, 3, 4, 5, 6, 7];
-
-// let ITEMS_LEFT_ARR = [0, 1, 2];
-// let ITEMS_RIGHT_ARR = [6, 7, 0];
-// let ITEMS_ACTIVE_ARR = [3, 4, 5];
-
-// const getActiveButtons = () => {
-//   return ITEMS_ACTIVE.querySelectorAll('.pets__card');
-// };
-
-// //create card depending id
-// const createCardTemplate = (num) => {
-//   return `<div class="pets__card" id="${num}">
-//             <div class="pets__card-img">
-//               <img src="${pets[num].img}" alt="" />
-//             </div>
-//             <p class="pets__card-title">${pets[num].petname}</p>
-//             <button class="pets__card-button">Learn more</button>
-//           </div>`;
-// };
-
-// //create group
-// const createGroupCardsTemplate = (arr, tag) => {
-//   arr.forEach((el) => {
-//     tag.insertAdjacentHTML('beforeend', createCardTemplate(el));
-//   });
-// };
-
-// createGroupCardsTemplate(ITEMS_LEFT_ARR, ITEMS_LEFT);
-// createGroupCardsTemplate(ITEMS_ACTIVE_ARR, ITEMS_ACTIVE);
-// createGroupCardsTemplate(ITEMS_RIGHT_ARR, ITEMS_RIGHT);
-
-// //add class, remove listener
-// const moveLeft = () => {
-//   CAROUSEL.classList.add('transition-left');
-//   BTN_LEFT.removeEventListener('click', moveLeft);
-//   BTN_RIGHT.removeEventListener('click', moveRight);
-// };
-
-// const moveRight = () => {
-//   CAROUSEL.classList.add('transition-right');
-//   BTN_RIGHT.removeEventListener('click', moveRight);
-//   BTN_LEFT.removeEventListener('click', moveLeft);
-// };
-
-// BTN_LEFT.addEventListener('click', moveLeft);
-// BTN_RIGHT.addEventListener('click', moveRight);
+render();
